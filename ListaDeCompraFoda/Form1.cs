cdbe648546dcedb3c2d8d74ce3bdec11dfce57b0
Igor Nery <igornery135@gmail.com>
2025-08-14 22:05:27 -0300

    Desenvolvimento da interface principal
    Criada a interface principal com o logotipo, o titulo, os botões e a lista com as compras.

--- ListaDeCompraFoda/Form1.cs
+++ ListaDeCompraFoda/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ListaDeCompraFoda
 {
-    public partial class Form1 : Form
+    public partial class ListaDeCompras : Form
     {
-        public Form1()
+        public ListaDeCompras()
         {
             InitializeComponent();
         }
--- ListaDeCompraFoda/Program.cs
+++ ListaDeCompraFoda/Program.cs
@@ -16,7 +16,7 @@ namespace ListaDeCompraFoda
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ListaDeCompras());
         }
     }
 }
